444ab55dffbd4403092e770d627e15dd13a61193
Victor Paulo da Silva Arruda <victor0070000@gmail.com>
2022-06-14 19:00:33 -0300

    exercicios de Map

---
+++ main.js
@@ -0,0 +1,32 @@
+// *** Primeiro exercício Map ***
+
+// const maca = {
+//   value: 2,
+// };
+
+// const laranja = {
+//   value: 8,
+// };
+
+// function mapComThis(arr, thisArg) {
+//   return arr.map(function (item) {
+//     return item * this.value;
+//   }, thisArg);
+// }
+
+// const num = [1, 2];
+
+// console.log("this -> maçã", mapComThis(num, maca));
+// console.log("this -> laranja", mapComThis(num, laranja));
+
+// *** Segundo exercício Map ***
+
+// function mapSemThis(arr) {
+//   return arr.map(function (item) {
+//     return item * 2;
+//   });
+// }
+
+// const nums = [2, 4, 6, 8, 10];
+
+// console.log(mapSemThis(nums));
