3a1f0c1a017a11a612eb050bf7bbdee02c94ae01
Victor Paulo da Silva Arruda <victor0070000@gmail.com>
2022-06-14 20:16:43 -0300

    Exercício Reduce

--- main.js
+++ main.js
@@ -1,48 +1,92 @@
 // *** Primeiro exercício Map ***
 
-// const maca = {
-//   value: 2,
-// };
+const maca = {
+  value: 2,
+};
 
-// const laranja = {
-//   value: 8,
-// };
+const laranja = {
+  value: 8,
+};
 
-// function mapComThis(arr, thisArg) {
-//   return arr.map(function (item) {
-//     return item * this.value;
-//   }, thisArg);
-// }
+function mapComThis(arr, thisArg) {
+  return arr.map(function (item) {
+    return item * this.value;
+  }, thisArg);
+}
 
-// const num = [1, 2];
+const num = [1, 2];
 
-// console.log("this -> maçã", mapComThis(num, maca));
-// console.log("this -> laranja", mapComThis(num, laranja));
+console.log("this -> maçã", mapComThis(num, maca));
+console.log("this -> laranja", mapComThis(num, laranja));
 
 // *** Segundo exercício Map ***
 
-// function mapSemThis(arr) {
-//   return arr.map(function (item) {
-//     return item * 2;
-//   });
-// }
+function mapSemThis(arr) {
+  return arr.map(function (item) {
+    return item * 2;
+  });
+}
 
-// const nums = [2, 4, 6, 8, 10];
+const nums = [2, 4, 6, 8, 10];
 
-// console.log(mapSemThis(nums));
+console.log(mapSemThis(nums));
 
-// *** Exercicio de Filter ***
+// *** Exercício de Filter ***
 
-// function filtraPares(arr) {
-//   return arr.filter(callback);
-// }
+function filtraPares(arr) {
+  return arr.filter(callback);
+}
 
-// function callback(item) {
-//   return item % 2 !== 0;
-// }
+function callback(item) {
+  return item % 2 !== 0;
+}
 
-// const meuArray = [1, 23, 55, 67, 30, 2, 4, 60, 51, 102];
+const meuArray = [1, 23, 55, 67, 30, 2, 4, 60, 51, 102];
 
-// console.log(filtraPares(meuArray));
+console.log(filtraPares(meuArray));
 
-// *** Exercicio de Reduce ***
+// *** Primeiro Exercício de Reduce ***
+
+function somaNumeros(arr) {
+  return arr.reduce(function (prev, current) {
+    return prev + current;
+  });
+}
+const arr = [1, 2];
+
+console.log(somaNumeros(arr));
+
+// *** Segundo Exercício de Reduce ***
+
+const lista = [
+  {
+    name: "sabão em pó",
+    preco: 17,
+  },
+  {
+    name: "cereal",
+    preco: 8.5,
+  },
+  {
+    name: "Toalha",
+    preco: 33,
+  },
+  {
+    name: "Carne",
+    preco: 41,
+  },
+];
+
+const saldoDisponivel = 100;
+
+function calculaSaldo(saldo, lista) {
+  return lista.reduce(function (prev, current, index) {
+    console.log("rodada", index + 1);
+    console.log({ prev });
+    console.log({ current });
+
+    return prev - current.preco;
+  }, saldoDisponivel);
+}
+
+console.log(calculaSaldo(saldoDisponivel, lista));
